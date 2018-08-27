da3f2515f27f57c3ba9b85f68ec5483abbff1a77
tvhoan2908 <tvhoan.2908@gmail.com>
2018-08-27 07:23:24 +0700

    Add Jquery nestable

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -11,42 +11,54 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <script src="{{ asset('js/app.js') }}" ></script>
+    <link rel="stylesheet" href="{{ asset('nestable/jquery.nestable.css') }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="{{asset('nestable/jquery.nestable.js')}}"></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script>
+        $(function () {
+            var updateOutput = function(e)
+            {
+                var list   = e.length ? e : $(e.target),
+                    output = list.data('output');
+                if (window.JSON) {
+                    output.val(window.JSON.stringify(list.nestable('serialize')));//, null, 2));
+                } else {
+                    output.val('JSON browser support required for this demo.');
+                }
+            };
+            $('#nestable3').nestable().on('change', updateOutput);;
+            updateOutput($('#nestable3').data('output', $('#nestable3-output')));
+        })
+    </script>
 </head>
 <body>
     <div id="app">
     <main class="py-4">
         <div class="container">
-            <div class="card">
-                <div class="row">
-                    <div class="card-body">
-                        @foreach ($shops as $shop)
-                            <div class="col-md-12">
-                                <h3>{{ $shop->category_name }}</h3>
-                                <hr />
-                                <div class="row">
-                                    @foreach ($shop->children as $cats)
-                                        <div class="col-md-4">
-                                            <h4>{{ $cats->category_name }}</h4>
-                                            <hr>
-                                            @foreach ($cats->children as $cat)
-                                                <h5>{{ $cat->category_name }}</h5>
-                                            @endforeach
-                                        </div>
-                                    @endforeach
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
+            <div class="dd" id="nestable3">
+                <?php
+                    $html = '';
+                    $traverse = function ($categories) use (&$traverse, &$html) {
+                        $html .= '<ol class="dd-list">';
+                        foreach ($categories as $category) {
+                            $html .= '<li class="dd-item dd3-item" data-id="'. $category->id .'" data-category_name="'. $category->category_name .'">';
+                            $html .= '<div class="dd-handle dd3-handle">Drag</div><div class="dd3-content">'. $category->category_name .'</div>';
+                                $traverse($category->children);
+                            $html .= '</li>';
+                            // echo '<div>'. PHP_EOL. $prefix . ' ' . $category->category_name . '</div>';
+                        }
+                        $html .= '</ol>';
+                    };
+
+                    $traverse($shops);
+                    echo $html;
+                ?>
             </div>
+            <textarea id="nestable3-output"></textarea>
         </div>
     </main>
     </div>
